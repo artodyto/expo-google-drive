6d402e6d5d456c0965dc7de9c6afa74bd35799de
Ardy Tuvillara <ardytuvillara@gmail.com>
2023-10-25 17:53:46 +0800

    build: added react-native google sign-in

---
+++ app.config.js
@@ -0,0 +1,33 @@
+export default {
+  expo: {
+    name: "expo-google-drive",
+    slug: "expo-google-drive",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "light",
+    splash: {
+      image: "./assets/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff",
+    },
+    assetBundlePatterns: ["**/*"],
+    ios: {
+      supportsTablet: true,
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#ffffff",
+      },
+    },
+    web: {
+      favicon: "./assets/favicon.png",
+    },
+    extra: {
+      eas: {
+        projectId: "8e4883de-fd74-408f-b807-3259770d36ed",
+      },
+    },
+  },
+};
